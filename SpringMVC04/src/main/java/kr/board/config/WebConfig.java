d8a1d486060cb65b60213e986086dab70c8dd0c2
ohhyerin <lyneen@naver.com>
2023-08-16 22:26:03 +0900

    commit

--- SpringMVC04/src/main/java/kr/board/config/WebConfig.java
+++ SpringMVC04/src/main/java/kr/board/config/WebConfig.java
@@ -1,5 +1,8 @@
 package kr.board.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -7,19 +10,32 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Class[] { RootConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Class[] { ServletConfig.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String[] { "/" };
 	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		encodingFilter.setForceEncoding(true);
+		
+		return new Filter[]{encodingFilter};
+
+	}
+	
+	
+	
 
 }
