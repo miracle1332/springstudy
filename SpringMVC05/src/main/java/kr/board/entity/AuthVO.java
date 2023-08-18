12a0dab8e8a19b78f445fafec2da73f32ba7de83
ohhyerin <lyneen@naver.com>
2023-08-18 13:46:27 +0900

    commit

--- SpringMVC05/src/main/java/kr/board/entity/AuthVO.java
+++ SpringMVC05/src/main/java/kr/board/entity/AuthVO.java
@@ -4,5 +4,7 @@ import lombok.Data;
 
 @Data
 public class AuthVO {
-
+	private int no; //일련번호
+	private String memID; //회원아이디
+	private String auth; //회원권한 (3가지부여 - ROLE_USER, ROLE_MANAGER, ROLE_ADMIN)
 }
